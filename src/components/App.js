8ee44bd77673269500431af1ab267eb22e943ad0
zoangrak <zoangrak@gmail.com>
2023-07-25 16:16:46 +0900

    Using Firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
-import AppRouter from "./Router";
+import { useState } from "react";
+import AppRouter from "components/Router";
+import { authService } from "fbase";
 
 function App() {
-  return <AppRouter />;
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; twitter {new Date().getFullYear()}</footer>
+    </>
+  );
 }
 
 export default App;
