3d30bf65269c79c88a6ed9828e3d85a77a15e4e7
zoangrak <zoangrak@gmail.com>
2023-07-25 16:33:40 +0900

    Hide Keys

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./components/App";
+import App from "components/App";
 import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
 console.log(firebase);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
