50ed4830c2bb880ae609b23f89c186a64637f251
zoangrak <zoangrak@gmail.com>
2023-07-28 03:05:20 +0900

    Log out

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/Navigation";
 
 const AppRouter = ({ isLoggedIn }) => {
   return (
     <Router>
+      {isLoggedIn && <Navigation />}
       <Routes>
         {isLoggedIn ? (
           <>
             <Route exact={true} path={"/"} element={<Home />} />
+            <Route exact={true} path={"/profile"} element={<Profile />} />
           </>
         ) : (
           <Route exact={true} path={"/"} element={<Auth />} />
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,3 +1,18 @@
 import React from "react";
+import { authService } from "fbase";
+import { useNavigate } from "react-router-dom";
 
-export default () => <span>Profile</span>;
+const Profile = () => {
+  const navigate = useNavigate();
+  const onLogOutClick = () => {
+    authService.signOut();
+    navigate("/");
+  };
+  return (
+    <>
+      <button onClick={onLogOutClick}>Log out</button>
+    </>
+  );
+};
+
+export default Profile;
